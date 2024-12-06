d0db047177520c666d7e6a53017dc3a3c6eeb3ff
Milos Zivlak <zivlakmilos@gmail.com>
2024-12-06 02:07:47 +0100

    Day 04 Puzzle 02

--- cmd/day05/main.go
+++ cmd/day05/main.go
@@ -40,6 +40,45 @@ func checkLine(rules map[int][]int, line string) int {
 	return res
 }
 
+func sortLine(rules map[int][]int, line string) int {
+	count := 0
+
+	parsed := strings.Split(line, ",")
+	data := make([]int, len(parsed))
+	for idx := range parsed {
+		data[idx], _ = strconv.Atoi(parsed[idx])
+	}
+
+	changed := true
+mainloop:
+	for changed {
+		changed = false
+		set := map[int]int{}
+
+		for idx, val := range data {
+			for _, val2 := range rules[val] {
+				if idx2, ok := set[val2]; ok {
+					val = val2
+					tmp := data[idx]
+					data[idx] = data[idx2]
+					data[idx2] = tmp
+					count++
+					changed = true
+					continue mainloop
+				}
+			}
+
+			set[val] = idx
+		}
+	}
+
+	if count == 0 {
+		return 0
+	}
+
+	return data[len(data)/2]
+}
+
 func solvePuzzle01() {
 	input := getInput()
 
@@ -64,6 +103,31 @@ func solvePuzzle01() {
 	fmt.Printf("Result: %d\n", res)
 }
 
+func solvePuzzle02() {
+	input := getInput()
+
+	res := 0
+
+	rules := map[int][]int{}
+
+	isParsingRules := true
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		if line == "" {
+			isParsingRules = false
+		}
+
+		if isParsingRules {
+			parseRule(rules, line)
+		} else {
+			res += sortLine(rules, line)
+		}
+	}
+
+	fmt.Printf("Result: %d\n", res)
+}
+
 func main() {
 	solvePuzzle01()
+	solvePuzzle02()
 }
