c2d1dc2f9c320050eef112104305286c17ab607b
Milos Zivlak <zivlakmilos@gmail.com>
2024-12-08 05:04:42 +0100

    Day 07 Puzzle 02

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -60,6 +60,66 @@ func solvePuzzle01() {
 	fmt.Printf("Total calibration results: %d\n", res)
 }
 
+func concate(a, b int) int {
+	var stack []int
+	for b > 0 {
+		stack = append(stack, b%10)
+		b /= 10
+	}
+
+	for i := len(stack) - 1; i >= 0; i-- {
+		a *= 10
+		a += stack[i]
+	}
+
+	return a
+}
+
+func checkIsValidWithConcat(target int, values []int) bool {
+	if len(values) == 0 {
+		return target == 0
+	}
+
+	if target <= 0 {
+		return false
+	}
+
+	val := values[len(values)-1]
+	if checkIsValidWithConcat(target-val, values[:len(values)-1]) {
+		return true
+	}
+	if target%val == 0 && checkIsValidWithConcat(target/val, values[:len(values)-1]) {
+		return true
+	}
+
+	for val > 0 {
+		if target%10 != val%10 {
+			return false
+		}
+		val /= 10
+		target /= 10
+	}
+
+	return checkIsValidWithConcat(target, values[:len(values)-1])
+}
+
+func solvePuzzle02() {
+	input := getInput()
+
+	res := 0
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		target, values := parseLine(line)
+		if checkIsValidWithConcat(target, values) {
+			res += target
+		}
+	}
+
+	fmt.Printf("Total calibration results: %d\n", res)
+}
+
 func main() {
 	solvePuzzle01()
+	solvePuzzle02()
 }
